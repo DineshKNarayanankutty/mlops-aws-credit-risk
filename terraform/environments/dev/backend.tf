1aa367fede2a28adabd9741c17d27f3ce5f8d3f0
Dinesh K Narayanan <dineshknarayanan.official@gmail.com>
2026-03-03 20:56:07 +0530

    chore: moved terraform infra to its own dedicated repo - https://github.com/DineshKNarayanankutty/mlops-aws-credit-risk-predict-platform.git

--- terraform/environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "mlops-aws-credit-risk-s3"
-    key          = "dev/terraform.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = true
-    encrypt      = true
-  }
-}
--- terraform/modules/alb_controller/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "irsa_role_arn" {
-  value = local.irsa_role_arn
-}
-
-output "helm_release_name" {
-  value = helm_release.this.name
-}
